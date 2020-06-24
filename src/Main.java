0d0796ecf8610867ccd387108b79f63f8deba56a
Jonathan Mai <23644860+TR1DENTSKY@users.noreply.github.com>
2020-06-24 14:52:42 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,3 @@ public class Main {
         Email em = new Email("John", "Wick");
     }
 }
-
-/*
-Email Application
-
-Scenario:
-You are an IT Support Administrator Specialist and are charged with the task of
-creating email accounts for new hires.
-
-Application should do the following:
-    * Generate an email with the following syntax:
-        <firstname.lastname@department.company.com>
-    * Determine the department (sales, development, accounting), if none leave blank
-    * Generate a random String for a password
-    * Display the name, department, and email
- */
